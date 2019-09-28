b9f57b24d4ca98727a361e32cb2b65314e565629
cardosolucas <cardosolucas61.lcs@gmail.com>
2019-09-28 13:32:37 -0300

    Update main class

--- marvin_cleaner/MarvinCleaner.py
+++ marvin_cleaner/MarvinCleaner.py
@@ -1,34 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 
-class MarvinCleaner:
+from preprocessor import one_hot_encoder
+from preprocessor import minmax
+from preprocessor import standard_scaler
 
+class MarvinCleaner(object):
+    
     def __init__(self):
-        dataframe = pd.DataFrame()
-
+        dataframe = pd.Dataframe()
+        
+    @classmethod
+    def load_csv(cls, filePath, missing_headers=False):
+        if missing_headers:
+            data = pd.read_csv(file_path, header=None)
+        else:
+            data = pd.read_csv(filePath, header=0)
+        cls.dataframe = data
+    
+    @classmethod
+    def describe(cls):
+        print(cls.dataframe.describe())
+    
     PIPELINE_OPTIONS = {
-        "imputation": imputation,
+        #"imputation": imputation,
         "one_hot_encoder": one_hot_encoder,
         "minmax": minmax,
-        "normalizer": normalizer,
-        "quantile_transformer": quantile_transformer,
-        "robust_scaler": robust_scaler,
+        #"variance_threshold": threshold,
+        #"normalizer": normalizer,
+        #"quantile_transformer": quantile_transformer,
+        #"robust_scaler": robust_scaler,
         "standard_scaler": standard_scaler,
     }
+    
+    @classmethod
+    def print_dataframe(cls):
+        print(cls.dataframe)
 
-    def load_csv(filePath, missing_headers=False):
-        if missing_headers:
-            data = read_csv(filePath, header=None)
-        else:
-            data = read_csv(filePath, header=0)
-
-        return data
-
-    def getData(url=False, path):
-        if not url:
-            self.dataframe = load_csv(path)
-
-    def preprocess(self, pipeline):
+    @classmethod
+    def preprocess(cls, pipeline):
         for stage in pipeline:
-            if stage in PIPELINE_OPTIONS:
+            if stage in cls.PIPELINE_OPTIONS:
                 print("Stage --> ", stage)
-                self.dataframe = PIPELINE_OPTIONS[stage](self.dataframe)
+                cls.dataframe = cls.PIPELINE_OPTIONS[stage](cls.dataframe)
+    
+        
